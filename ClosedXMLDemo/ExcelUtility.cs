8684b4878635d0d7d4033f419c04d5c07b374667
sholfen <sholfen@gmail.com>
2014-06-30 01:10:37 +0800

    change code

---
+++ ClosedXMLDemo/ExcelUtility.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace ClosedXMLDemo
+{
+    public class ExcelUtility
+    {
+        public static DataTable ConvertObjectsToDataTable(IEnumerable<object> objects)
+        {
+            DataTable dt = null;
+
+            if (objects != null && objects.Count() > 0)
+            {
+                Type type = objects.First().GetType();
+                dt = new DataTable(type.Name);
+
+                foreach (PropertyInfo property in type.GetProperties())
+                {
+                    dt.Columns.Add(new DataColumn(property.Name));
+                }
+
+                foreach (FieldInfo field in type.GetFields())
+                {
+                    dt.Columns.Add(new DataColumn(field.Name));
+                }
+
+                foreach (object obj in objects)
+                {
+                    DataRow dr = dt.NewRow();
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        PropertyInfo propertyInfo = type.GetProperty(column.ColumnName);
+                        if (propertyInfo != null)
+                        {
+                            dr[column.ColumnName] = propertyInfo.GetValue(obj, null);
+                        }
+
+                        FieldInfo fieldInfo = type.GetField(column.ColumnName);
+                        if (fieldInfo != null)
+                        {
+                            dr[column.ColumnName] = fieldInfo.GetValue(obj);
+                        }
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+
+            return dt;
+        }
+
+        public static void ExportExcelFromDataTable(DataTable dt, string fileName)
+        {
+            XLWorkbook workbook = new XLWorkbook();
+            workbook.AddWorksheet(dt, "Sheet1");
+            workbook.SaveAs(fileName);
+        }
+    }
+}
--- ClosedXMLDemo/Program.cs
+++ ClosedXMLDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,20 @@ namespace ClosedXMLDemo
     {
         static void Main(string[] args)
         {
+            List<TestClass> list = new List<TestClass>();
+            list.Add(new TestClass { Prop1 = "p1", Prop2 = "p2", Field1 = "f1", Field2 = "f2" });
+            DataTable dt = ExcelUtility.ConvertObjectsToDataTable(list);
+            ExcelUtility.ExportExcelFromDataTable(dt, "test.xlsx");
+            Console.WriteLine();
         }
+    }
+
+    public class TestClass
+    {
+        public string Prop1 { get; set; }
+        public string Prop2 { get; set; }
+
+        public string Field1;
+        public string Field2;
     }
 }
